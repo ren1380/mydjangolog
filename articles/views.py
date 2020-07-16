bc889f3c46a1fe1dee7ad89371472aa00f0dcee2
ren1380 <baranbahari8090@gmail.com>
2020-07-16 12:44:57 +0430

    commit url

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from . import models
 # Create your views here.
 
@@ -6,3 +6,6 @@ def articles_list(request):
     articles = models.Articles.objects.all().order_by('date')
     args = {'articles':articles}
     return render(request, 'articles/articles_list.html', args)
+
+def articles_detail(request, slug):
+    return(HttpResponse(slug))
